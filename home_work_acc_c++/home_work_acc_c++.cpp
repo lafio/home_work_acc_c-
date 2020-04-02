fac4ddb82f55e884d4a3e6656eda0856c2f85979
lichengxiao1995 <lichengxiao1995@126.com>
2020-04-02 15:29:28 +0800

    习题 3-2

--- home_work_acc_c++/home_work_acc_c++.cpp
+++ home_work_acc_c++/home_work_acc_c++.cpp
@@ -5,18 +5,11 @@
 #include <algorithm>
 #include <vector>
 
+//习题3-2 
+
 using namespace std;
 int main()
 {
-	cout << "Please input your name:";
-	string name;
-	cin >> name;
-	cout << "Hello, " << name << "!" << endl;
-	cout << "Please enter your midterm and final exam grades:";
-	double midterm, final;
-	cin >> midterm >> final;
-	cout << "Enter all your homework grades,"
-		"followed by end-of-file:";
 	double x;
 	vector<double> homework;
 	while (cin >> x) {
@@ -24,19 +17,24 @@ int main()
 	}
 	typedef vector<double>::size_type vec_sz;
 	vec_sz size = homework.size();
-	if (size == 0) {
-		cout << endl << "You must enter your grades."
-			"Please try again." << endl;
+	int num;
+	if (size < 4) {
+		cout << "You must enter more than 4 numbers!" << endl;
 		return 1;
+	}else if(size % 4 != 0) {
+		cout << "Count of your numbers must be 4 "
+			"or multiple of 4!" << endl;
+		return 1;
+	}else {
+		num = size / 4;
 	}
-	sort(homework.begin(),homework.end());
-	vec_sz mid = size / 2;
-	double median;
-	median = size % 2 == 0 ? (homework[mid] + homework[mid - 1]) / 2
-		: homework[mid];
-	streamsize prec = cout.precision();
-	cout << "Your final grade is " << setprecision(3)
-		<< 0.2 * midterm + 0.4 * final + 0.4 * median
-		<< setprecision(prec) << endl;
+	sort(homework.begin(),homework.end(),greater<double>());
+	for (int i = 0; i < size; i+=num) {
+		for (int j = 0; j < num; ++j) {
+			cout << homework[i + j] << "," ;
+		}
+		cout <<  endl;
+	}
+	
 	return 0;
 }
